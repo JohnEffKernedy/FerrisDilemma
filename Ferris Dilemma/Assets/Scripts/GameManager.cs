aed24a6a946dc7410fcdb4d4c207f65efe66f959
JohnEffKernedy <florian.kaindl@stud.uni-regensburg.de>
2018-10-13 18:58:37 +0200

    Moving Clouds

--- Ferris Dilemma/Assets/Scripts/GameManager.cs
+++ Ferris Dilemma/Assets/Scripts/GameManager.cs
@@ -9,6 +9,8 @@ public class GameManager : MonoBehaviour {
     public Text scoreText;
     public Text badGuyText;
     private GameObject[] carts;
+    public GameObject Skybox;
+    public float skyspeed = 3f;
 
     public Transform badGuy;
     int guests = 0;
@@ -22,6 +24,15 @@ public class GameManager : MonoBehaviour {
 	void Update () {
         guests = 0;
         badGuys = 0;
+
+        Vector3 pos = Camera.main.WorldToViewportPoint(Skybox.transform.position);
+        if (1.0 < pos.x)
+        {
+            Skybox = Instantiate(Skybox);
+        }
+        Skybox.transform.Translate(new Vector3(skyspeed*Time.deltaTime, 0, 0));
+       
+
 		for(int i = 0; i < carts.Length; i++)
         {
             guests += carts[i].GetComponent<WheelCart>().counter;
