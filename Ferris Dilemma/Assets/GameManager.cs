b66053028582b6ad0996435905c5dc817f97ec56
JohnEffKernedy <florian.kaindl@stud.uni-regensburg.de>
2018-10-13 11:52:49 +0200

    Ferris Scripts

---
+++ Ferris Dilemma/Assets/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    GameObject[] carts;
+
+	// Use this for initialization
+	void Start () {
+        carts = GameObject.FindGameObjectsWithTag("Cart");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Ferris Dilemma/Assets/Scripts/ferriswheel.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ferriswheel : MonoBehaviour {
+
+    public float rotationSpeed = 2;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.forward, -rotationSpeed);
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.forward, rotationSpeed);
+        }
+	}
+}
