862740f2d2794d7e51b88085dcb92d8e361b7bad
JohnEffKernedy <florian.kaindl@stud.uni-regensburg.de>
2018-10-14 12:56:59 +0200

    Change Impact Pitch

--- Ferris Dilemma/Assets/Scripts/WheelCart.cs
+++ Ferris Dilemma/Assets/Scripts/WheelCart.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 public class WheelCart : MonoBehaviour {
     public int counter = 0;
     public int badGuys = 0;
+
+    public AudioSource PunchSound;
+
+    private float originalPitch;
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
@@ -17,12 +21,15 @@ public class WheelCart : MonoBehaviour {
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        
         if (collision.CompareTag("BadGuy"))
         {
+            PlayPunchSound(collision.GetComponent<Rigidbody2D>().velocity.magnitude);
             badGuys++;
         }
         if (collision.CompareTag("Guest"))
         {
+            PlayPunchSound(collision.GetComponent<Rigidbody2D>().velocity.magnitude);
             counter++;
         }
     }
@@ -39,4 +46,14 @@ public class WheelCart : MonoBehaviour {
             counter--;
         }
     }
+
+    private void PlayPunchSound(float velocity)
+    {
+        if (PunchSound.isPlaying) return;
+        if (velocity < 8) return;
+        velocity = Mathf.Clamp(velocity, 8, 16);
+        float pitchChange = 0.5f + (velocity / 16f);
+        PunchSound.pitch = pitchChange;
+        PunchSound.Play();
+    } 
 }
